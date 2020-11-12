63b58f6e357b70edc1e489ecf778f1972a6043d2
Mike Lang <mikelang3000@gmail.com>
2020-11-13 10:01:17 +1100

    Update macros

--- mikeirc/main.py
+++ mikeirc/main.py
@@ -57,8 +57,8 @@ KEYWORD_HIGHLIGHTS = {
 	'DBVideoStrikeTeam': NICK_HIGHLIGHT,
 }
 USER_REGEX_HIGHLIGHTS = {
-	'DB_.*': '1',
-	'.*_LRR': '1',
+	'DB_\w*': '1',
+	'\w*_LRR': '1',
 }
 
 EXCLUDE_NUMERICS = {5}
@@ -66,17 +66,17 @@ EXCLUDE_NUMERICS = {5}
 # for VST
 MACROS = {
 	"videos":
-		"Missed any of DB2019 and want to catch up? The VST has you covered, with full event logs and youtube videos. Check it out! https://db13.vst.ninja",
+		"Missed any of DB2020 and want to catch up? The VST has you covered, with full event logs and youtube videos. Check it out! https://vst.ninja/DB14/",
 	"poster":
-		"Miss a poster update? Check out https://vst.ninja/DB13/posters/ to see the poster versions as it grows! Or check https://vst.ninja/DB13/postermap to see the clips that the poster references! Here's the latest poster: https://vst.ninja/DB13/posters/latest.png",
+		"Miss a poster update? Check out https://vst.ninja/DB14/posters/ to see the poster versions as it grows! Or check https://vst.ninja/DB14/postermap to see the clips that the poster references!",
 	"stats":
-		"Fun stats generated from the chat, for all your inner math nerd needs! http://chatstats.vst.ninja",
+		"Fun stats generated from the chat, for all your inner math nerd needs! https://vst.ninja/chatstats/",
 	"milestones":
 		"Want to see how close we are to various milestones? Check out https://vst.ninja/milestones/",
 	"gifs":
-		"Our Giffers work around the clock to make you say 'I remember that!' or 'Wait, when did that happen?'. Check out their efforts at http://greywool.com/desertbus/2019/gifs/",
+		"Our Giffers work around the clock to make you say 'I remember that!' or 'Wait, when did that happen?'. Check out their efforts at http://greywool.com/desertbus/2020/gifs/",
 	"picnic":
-		"Hey folks. While you *calmly* wait for the stream's inevitable return, why not take the opportunity to catch up on some stuff you missed? https://vst.ninja/DB13/",
+		"Hey folks. While you *calmly* wait for the stream's inevitable return, why not take the opportunity to catch up on some stuff you missed? https://vst.ninja/DB14/",
 	"missing":
 		"Have you been a DesertBus fan for a long time? Do you like to hoard things like video captures? See if you can help us complete our archive at http://vst.ninja/missingdata/",
 }
