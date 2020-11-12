be4ec24cde81e302ceb904a0938cd0fa58606592
Mike Lang <mikelang3000@gmail.com>
2020-11-13 10:26:43 +1100

    macro tweak

--- mikeirc/main.py
+++ mikeirc/main.py
@@ -78,7 +78,7 @@ MACROS = {
 	"donor":
 		"Don't forget to sign up for a donor account at https://desertbus.org/donor/sign-up so you can win Giveaways with your donations! And link that twitch account so you can bid on Live Auctions!",
 	"vst":
-		"The VST, or Video Strike Team, is a group of people that catalog, capture, clip, and upload moments from desertbus to youtube. You can find our website at https://vst.ninja or our youtube page at https://www.youtube.com/channel/UCz5-PNQxaT4WtB_OMAwD85g"
+		"The VST, or Video Strike Team, is a group of people that catalog, capture, clip, and upload moments from desertbus to youtube. You can find our website at https://vst.ninja or our youtube page at https://www.youtube.com/DesertBusForHope"
 	"picnic":
 		"Hey folks. While you *calmly* wait for the stream's inevitable return, why not take the opportunity to catch up on some stuff you missed? https://vst.ninja/DB14/",
 	"missing":
